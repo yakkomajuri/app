900a237d0d23228a255de9ac079aaa8576af4fbf
yakkomajuri <yakko.majuri@gmail.com>
2021-03-26 12:56:56 +0000

    add logging

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ const probotServer = new ProbotServer((app) => {
         const members = await organizationMembers.getOrganizationMembers()
 
         // Only org members can request contributors be added
+
+        console.log(context.payload.issue.user.login, Array.from(members))
         if (!members.has(context.payload.issue.user.login)) {
             return
         }
